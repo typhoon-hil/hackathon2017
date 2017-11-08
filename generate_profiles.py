0dce4ab20ea034facb0f0fa425e9673306f5bda8
= <=>
2017-11-08 16:40:12 +0100

    Added ranking (results) file and competition profiles

--- generate_profiles.py
+++ generate_profiles.py
@@ -8,14 +8,26 @@ __license__ = "MIT"
 
 
 def generate_profiles():
-    LOAD_SCALES = [1.0, 1.1, 0.8, 1.2, 0.9]
-    SOLAR_SCALES = [1.3, 0.4, 0.8, 0.9, 1.1]
-    BLACKOUTS = [[[11, 11.75]],
+    # Training profiles
+    #LOAD_SCALES = [1.0, 1.1, 0.8, 1.2, 0.9]
+    #SOLAR_SCALES = [1.3, 0.4, 0.8, 0.9, 1.1]
+    #BLACKOUTS = [[[11, 11.75]],
+    #             [],
+    #             [[2.5,3]],
+    #             [[20,21]],
+    #             [],
+    #             ]
+
+    # Competition profiles
+    LOAD_SCALES = [1.0, 0.8, 0.9, 1.1, 1.2]
+    SOLAR_SCALES = [1.0, 1.5, 1.2, 0.9, 0.5]
+    BLACKOUTS = [[[22.75,23.25]],
                  [],
-                 [[2.5,3]],
-                 [[20,21]],
-                 [],
-                ]
+                 [[13.0,14.0]],
+                 [[2.5,3.25]],
+                 [[18.5,19.25]],
+                 ]
+
 
     PROFILES = []
 
@@ -32,10 +44,10 @@ def generate_profiles():
         PROFILES += profile
         LOAD_SCALING_PREV = LOAD_SCALES[n]
 
-    with open(CFG.profile_file, 'w') as f:
+    with open('./data/profiles_cmp.json', 'w') as f:
         f.write(json.dumps(PROFILES))
 
-    print('Profile is generated in {}'.format(CFG.profile_file))
+    print('Profile is generated in {}'.format('./data/profiles_cmp.json'))
 
 
 if __name__ == '__main__':
